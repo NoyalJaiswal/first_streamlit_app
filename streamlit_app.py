726e9c051b93aa273f8ad1ef98b6de42e4ce8324
NoyalJaiswal <154743382+NoyalJaiswal@users.noreply.github.com>
2023-12-24 19:44:30 +0530

    Updated menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, spinach & rocket smoothie')
+streamlit.text('Hard-boiled Free-Range eggs')
