574c2262edb47a6adab9858c449c1191e19a1eed
NoyalJaiswal <154743382+NoyalJaiswal@users.noreply.github.com>
2023-12-24 19:47:10 +0530

    new menu added along with icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, spinach & rocket smoothie')
-streamlit.text('Hard-boiled Free-Range eggs')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale, spinach & rocket smoothie')
+streamlit.text(' 🐔 Hard-boiled Free-Range eggs')
+streamlit.text('🥑🍞Avacado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
